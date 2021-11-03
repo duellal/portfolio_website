050dbfc321f593d624b0f98e33056cfee10c6412
Jessica Duell <jessica.duell@pendo.io>
2021-11-03 19:58:35 -0400

    Updated Landing page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Alexandria Duell</h1>
+  <div id="links">
+    <a href="https://github.com/duellal" target="blank">Github</a>
+    <a href="https://www.linkedin.com/in/alexandria-duell/" target="blank">LinkedIn</a>
+    <a href="https://docs.google.com/document/d/e/2PACX-1vQXqcpBmogmYxzdWDyFVSgJa9IWZpmJMYPfiG9U3OSBHfXR9edoZeswjbBEvVWuG31Gfemmy11Dbo8Z/pub" target="blank">Resume</a>
+    <a href="">Email</a>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'Portfolio',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: black;
+  color: white;
 }
+
+a {
+  color: white;
+  text-decoration: none;
+  padding: 3%;
+}
+
+#app{
+  padding-top: 3%;
+}
+
+#links {
+  display: flex;
+  justify-content: center;
+}
+
 </style>
